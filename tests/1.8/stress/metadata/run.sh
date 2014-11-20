307ec4c7aad8557429480b1b727366274716d9a7
Philippe Proulx <eeppeliteloop@gmail.com>
2014-11-19 19:25:47 -0500

    Sort list of directories in run.sh scripts
    This mimics the behaviour of Bash's `*/`, which is to
    sort the results. Tests are executed in a deterministic
    order.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- tests/1.8/stress/metadata/run.sh
+++ tests/1.8/stress/metadata/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for dir in $(find -mindepth 1 -maxdepth 1 -type d); do
+for dir in $(find -mindepth 1 -maxdepth 1 -type d | sort); do
 	echo "Running ${dir} tests"
 	cd ${dir}
 	./run.sh
